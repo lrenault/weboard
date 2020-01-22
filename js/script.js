0a14d2422e01c2b5c458c73fc8c4c62cde2bdd78
Lenny Renault <lrenault@touriste-147-4.ircam.fr>
2020-01-22 19:12:49 +0100

    Create index.html and request audio stream

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const context = new AudioContext();
+
+if (context.state == 'suspended') {
+  await context.resume();
+}
+
+//const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+// Suppress sound optimization of the browser
+const stream = await navigator.mediaDevices.getUserMedia({
+  audio: {
+    echoCancellation: false,
+    autoGainControl: false,
+    noiseSuppression: false,
+    latency: 0
+  }
+});
+
+// Permission status
+const micStatus = await navigator.permissions.query({name: 'microphone'});
+console.log(micStatus); // state "prompt"
+// Reset permission to inital state.
+await navigator.permissions.revoke({name: 'microphone'});
+
+const lineInSource = context.createMediaStreamSource(stream);
+lineInSource.connect(context.destination);
+
+// Gain control
+const gainNode = new GainNode(context, {gain: 0.5});
+// disconnect source before constructing a new graph
+lineInSource.disconnect();
+// connect nodes
+lineInSource.connect(gainNode).connect(context.destination);
+
+// Increase volume
+gainNode.gain.value = 2;
