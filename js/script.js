c73e0e2b48caa3a3f7e950340ed96e2061380200
Lenny Renault <lrenault@MacBook-Pro-de-Lenny.local>
2020-01-23 19:46:06 +0100

    Move backend to Pizzicato

--- js/script.js
+++ js/script.js
@@ -1,38 +1,49 @@
-import resumeContext from './resumeContext.js';
+// 1. declare frequency max
+let freqMax = 20000;
+let init = false;
+let voice;
 
-async function init() {
-  const audioContext = new AudioContext();
-  // resume audio context
-  await resumeContext(audioContext);
+// 3. declare ping pong delay
+let pingPongDelay = new Pizzicato.Effects.PingPongDelay({
+  feedback: 0.6,
+  time: 0.4,
+  mix: 0.5
+});
 
-  //const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-  // Suppress sound optimization of the browser
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: {
-      echoCancellation: false,
-      autoGainControl: false,
-      noiseSuppression: false,
-      latency: 0
-    }
-  });
+// 4. declare low pass filter
+let lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+  frequency: 1000,
+  peak: 10,
+});
 
-  // Permission status
-  const micStatus = await navigator.permissions.query({name: 'microphone'});
-  console.log(micStatus); // state "prompt"
-  // Reset permission to inital state.
-  await navigator.permissions.revoke({name: 'microphone'});
+// 5. declare high pass filter
+let highPassFilter = new Pizzicato.Effects.HighPassFilter({
+  frequency: 10,
+  peak: 10,
+});
 
-  const lineInSource = context.createMediaStreamSource(stream);
-  lineInSource.connect(context.destination);
 
-  // Gain control
-  const gainNode = new GainNode(context, {gain: 0.5});
-  // disconnect source before constructing a new graph
-  lineInSource.disconnect();
-  // connect nodes
-  lineInSource.connect(gainNode).connect(context.destination);
-
-  // Increase volume
-  gainNode.gain.value = 2;
-
+var instrumentConnected = document.querySelector('input[id="connectSource"]');
+instrumentConnected.onchange = function() {
+  if (instrumentConnected.checked) {
+    instru = new Pizzicato.Sound({ source : 'input'}, function() {
+      instru.play();
+      instru.addEffect(pingPongDelay);
+      instru.addEffect(lowPassFilter);
+      instru.addEffect(highPassFilter);
+    })
+  } else {
+    instru.stop();
+  }
 }
+
+document.body.addEventListener('mousemove', function(event) {
+  // 9. vary ping pong delay on X axis
+  pingPongDelay.time = event.pageX / document.body.clientWidth;
+
+  // 10. vary low pass filter on Y axis
+  lowPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax;
+
+  // 11. vary high pass filter on Y axis
+  highPassFilter.frequency = event.pageY / document.body.clientHeight * freqMax;
+});
